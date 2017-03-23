a00d0f9627dd78949f28c0a01dc1e551db582b1b
frankg26 <frankg2694@hotmail.com>
2017-03-23 14:47:16 -0600

    Adjusted Hearts

--- WpfApplication1/WpfApplication1/CubaLibreProfile.xaml.cs
+++ WpfApplication1/WpfApplication1/CubaLibreProfile.xaml.cs
@@ -35,16 +35,6 @@ namespace WpfApplication1
 
         private void Fav_Click(object sender, RoutedEventArgs e)
         {
-            
-            if (Heart.Text == "❤ 37")
-            {
-                Heart.Text = "♥ 38";
-            }
-
-            else
-            {
-                Heart.Text = "❤ 37";
-            }
 
         }
 
