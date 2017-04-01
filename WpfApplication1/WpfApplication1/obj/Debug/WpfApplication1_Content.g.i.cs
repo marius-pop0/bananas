9cb5eef1b43b97b5defdd9e2d34dd4234efa150b
frankg26 <frankg2694@hotmail.com>
2017-04-01 09:56:13 -0600

    Moved Combo Box to the left a bit

--- WpfApplication1/WpfApplication1/obj/Debug/WpfApplication1_Content.g.i.cs
+++ WpfApplication1/WpfApplication1/obj/Debug/WpfApplication1_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/gmargarita.jpg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/plus.jpg")]
 
 
